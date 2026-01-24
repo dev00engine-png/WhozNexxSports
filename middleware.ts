be5e79bb96663e9b66360049ae3d777287532efb
WhozNexxSports Admin <admin@whoznexxsports.com>
2026-01-24 09:31:22 -0800

    Temporarily disable middleware matcher for production troubleshooting

--- middleware.ts
+++ middleware.ts
@@ -71,15 +71,4 @@ export async function middleware(req: NextRequest) {
   return response
 }
 
-export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * Feel free to modify this pattern to include more paths.
-     */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-}
+export const config = { matcher: [] }
